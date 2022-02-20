17638300c3f9ddfaf65c7be0f3b12019e08a350d
Thiago <thiagofp707@gmail.com>
2022-02-20 02:25:31 -0300

    [startup](Request Types): Implementing the responses for the each request type

--- AlexaBirthdayTracker/Controllers/AlexaController.cs
+++ AlexaBirthdayTracker/Controllers/AlexaController.cs
@@ -1,4 +1,5 @@
 using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
 using Alexa.NET.Response;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,7 +15,32 @@ namespace AlexaBirthdayTracker.Controllers
             SkillResponse output = new SkillResponse();
             output.Version = "1.0";
             output.Response = new ResponseBody();
-            output.Response.OutputSpeech = new PlainTextOutputSpeech("Hello this works");
+
+            switch (input.Request.Type)
+            {
+                case "LaunchRequest":
+                    output.Response.OutputSpeech = new PlainTextOutputSpeech("Hello! Welcome to birthday tracker! May i help you tracking birthdays? 1");
+                    output.Response.ShouldEndSession = false;
+                    break;
+                case "SessionEndedRequest":
+                    output.Response.OutputSpeech = new PlainTextOutputSpeech("Hello! Welcome to birthday tracker! May i help you tracking birthdays? 2");
+                    output.Response.ShouldEndSession = false;
+                    break;
+                case "IntentRequest":
+                    IntentRequest intentRequest = (IntentRequest)input.Request;
+                    switch (intentRequest.Intent.Name)
+                    {
+                        case "next_birthday_intent":
+                            output.Response.OutputSpeech = new PlainTextOutputSpeech("The next birthday is of John on 24nd February 2022");
+                            output.Response.ShouldEndSession = false;
+                            break;
+                        case "AMAZON.FallbackIntent":
+                            output.Response.OutputSpeech = new PlainTextOutputSpeech("Sorry. Can you repeat that?");
+                            output.Response.ShouldEndSession = false;
+                            break;
+                    }
+                    break;
+            }
             return output;
         }
     }
